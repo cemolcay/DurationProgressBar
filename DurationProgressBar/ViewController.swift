c3a236b8fe5766f03dc9ec9f82bd3e8e4b2e2fc2
Cem Olcay <ccemolcay@gmail.com>
2022-02-09 13:08:19 +0300

    Initial Commit

---
+++ DurationProgressBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DurationProgressBar
+//
+//  Created by Cem Olcay on 2/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
